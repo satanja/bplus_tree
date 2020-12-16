00189c814c7955d383d489a2efb5b96cf10c83cb
satanja <s.a.tanja@student.tue.nl>
2020-12-16 14:39:20 +0100

    Add randomized queries

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <chrono>
 #include <map>
+#include <random>
 #include "bplus_tree.h"
 
-#define SIZE 200000
+#define SIZE 5000000
+
+void build_query(int size, std::vector<int> &queries)
+{
+    queries.resize(size);
+    for (int i = 0; i < size; i++)
+    {
+        queries[i] = i;
+    }
+    std::shuffle(queries.begin(), queries.end(), std::default_random_engine(0));
+}
 
 int main()
-{
-    bplus_tree<int, int> btree(25);
+{   
+    std::vector<int> queries;
+    build_query(SIZE, queries);
+
+    bplus_tree<int, int> btree(50);
     std::map<int, int> dict;
     
     std::cout << "N = " << SIZE << std::endl;
     auto begin = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < SIZE; i++)
+    for (int i : queries)
     {
         dict.insert(std::make_pair(i, i));
     }
@@ -21,7 +35,7 @@ int main()
     std::cout << "map init: " << duration.count() << "ms" << std::endl;
 
     begin = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < SIZE; i++)
+    for (int i : queries)
     {
         btree.insert(i, i);
     }
@@ -31,7 +45,7 @@ int main()
 
     begin = std::chrono::high_resolution_clock::now();
     uint64_t hits1 = 0;
-    for (int i = 0; i < SIZE; i++)
+    for (int i : queries)
     {
         hits1 += dict.contains(i);
     }
@@ -41,11 +55,12 @@ int main()
 
     begin = std::chrono::high_resolution_clock::now();
     uint64_t hits2 = 0;
-    for (int i = 0; i < SIZE; i++)
+    for (int i : queries)
     {
         hits2 += btree.contains(i);
     }
     end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration<double, std::milli>(end - begin);
     std::cout << "btree search: " << duration.count() << "ms" << std::endl;
+    std::cout << (hits1 == hits2) << std::endl;
 }
